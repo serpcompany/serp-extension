ddafa19487683121867f235064c013cc20fc1c39
devinschumacher <devin@serp.co>
2024-02-23 03:55:29 -0800

    working!

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ Extension functionality:
 
 1. run the `js/medium.js` file when user clicks the extension icon and is on any page of the domain `medium.com`
 2. run the `js/reddit.js` file when user clicks the extension icon and is on any page of the domain `reddit.com`
-
+3. define the `amazonTag` in the `js/amazon.js` file
--- js/amazon.js
+++ js/amazon.js
@@ -1,21 +1,39 @@
 // js/amazon.js
+const amazonTag = 'serpext-20';
+
 function updateAmazonLinks() {
-    document.querySelectorAll('a[href*="amazon.com"]').forEach(link => {
+  document.querySelectorAll('a[href*="amazon.com"]').forEach(link => {
+    // Modify the href attribute immediately
+    try {
+      let url = new URL(link.href);
+      let params = url.searchParams;
+      if (params.has('tag')) {
+        params.set('tag', amazonTag);
+      } else {
+        params.append('tag', amazonTag);
+      }
+      link.href = url.toString();
+    } catch (error) {
+      console.error(`Failed to modify URL: ${link.href}`);
+      console.error(error);
+    }
+
+    // Add a 'click' event listener to modify the href attribute when the link is clicked
+    link.addEventListener('click', event => {
       try {
         let url = new URL(link.href);
         let params = url.searchParams;
-        if (params.has('tag')) {
-          params.set('tag', 'serpextension-20');
-        } else {
-          params.append('tag', 'serpextension-20');
+        if (!params.has('tag')) {
+          params.append('tag', amazonTag);
+          link.href = url.toString();
         }
-        link.href = url.toString();
       } catch (error) {
         console.error(`Failed to modify URL: ${link.href}`);
         console.error(error);
       }
     });
-  }
-  
-  // Run updateAmazonLinks every 2 seconds
-  setInterval(updateAmazonLinks, 2000);
+  });
+}
+
+// Run updateAmazonLinks every 2 seconds
+setInterval(updateAmazonLinks, 2000);
