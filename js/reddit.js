5cf6ee02e91f9fe4e47abc1230c4b17d48308a5c
Yashu Mittal <mittalyashu77@gmail.com>
2024-03-20 20:16:15 +0530

    feat: update logic for reddit

--- js/reddit.js
+++ js/reddit.js
@@ -1,30 +1,45 @@
+var __clapper_intervalId;
+var __clapper_interval_speed;
+
+chrome.storage.local.get(["speed"], function (result) {
+  __clapper_interval_speed = result.speed;
+});
+
 function redditScript() {
-    function randomDelay(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
+  function randomDelay(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 
-    function upvote() {
-        const buttons = document.querySelectorAll('[data-click-id="upvote"]');
-        buttons.forEach((b, i) => {
-            setTimeout(() => {
-                const rect = b.getBoundingClientRect();
-                if (rect.top >= 0) { // Only scroll to the button if it's below the current scroll position
-                    b.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
-                }
-                if (b.getAttribute("aria-pressed") == "true") {
-                    console.log("already upvoted...");
-                } else if (b.getAttribute("aria-pressed") == "false") { 
-                    b.click();
-                    console.log("upvoting...");
-                } else {
-                    console.log("something went wrong...");
-                }      
-            }, i * randomDelay(1000, 3000)); // Random delay between X and X seconds
-        });
-    }
+  function upvote() {
+    const buttons = document.querySelectorAll('[data-click-id="upvote"]');
+    buttons.forEach((b) => {
+      setTimeout(() => {
+        const rect = b.getBoundingClientRect();
+        if (rect.top >= 0) {
+          // Only scroll to the button if it's below the current scroll position
+          b.scrollIntoView({ behavior: "smooth", block: "nearest" });
+        }
+        if (b.getAttribute("aria-pressed") == "true") {
+          console.log("already upvoted...");
+        } else if (b.getAttribute("aria-pressed") == "false") {
+          b.click();
+          console.log("upvoting...");
+        } else {
+          console.log("something went wrong...");
+        }
+      }, __clapper_interval_speed); // Random delay between X and X seconds
+    });
+  }
 
-    upvote();
-    setInterval(upvote, randomDelay(2000, 8000)); // Run upvote function every X to X seconds
+  upvote();
+  // Run upvote function every X to X seconds
+  __clapper_intervalId = setInterval(upvote, randomDelay(2000, 8000));
 }
 
 redditScript();
+
+chrome.runtime.onMessage.addListener((request) => {
+  if (request.message === "___clapper_stop_script") {
+    clearInterval(__clapper_intervalId);
+  }
+});
--- popup.js
+++ popup.js
@@ -1,13 +1,85 @@
+// 1 seconds (1000 ms)
+const defaultSpeed = 1000;
+
 // Read speed from Chrome storage
-chrome.storage.local.get(['speed'], function (result) {
-  const speedInput = document.getElementById('speedInput');
-  speedInput.value = result.speed;
+chrome.storage.local.get(["speed"], function (result) {
+  const speedInput = document.getElementById("speedInput");
+
+  if (result?.speed === undefined) {
+    speedInput.value = defaultSpeed;
+    chrome.storage.local.set({ speed: defaultSpeed }, function () {
+      console.log("Speed is set to " + defaultSpeed);
+    });
+  } else {
+    speedInput.value = result.speed;
+  }
 });
 
 // Set speed from the Popup
-document.getElementById('setSpeed').onclick = function () {
-  var speed = document.getElementById('speedInput').value;
-  chrome.storage.local.set({speed: speed}, function () {
-    console.log('Speed is set to ' + speed);
+document.getElementById("setSpeed").onclick = function () {
+  var speed = document.getElementById("speedInput").value;
+  chrome.storage.local.set({ speed: speed }, function () {
+    console.log("Speed is set to " + speed);
   });
+};
+
+const redditBtn = document.getElementById("redditBtn");
+redditBtn.onclick = startReddit;
+const mediumBtn = document.getElementById("mediumBtn");
+// const amazonBtn = document.getElementById("amazonBtn");
+
+let activePlatform;
+chrome.storage.local.get(["activePlatform"], function (result) {
+  resetBtnState();
+
+  activePlatform = result.activePlatform;
+  if (activePlatform === "reddit") {
+    redditBtn.removeAttribute("disabled");
+  } else if (activePlatform === "medium") {
+    mediumBtn.removeAttribute("disabled");
+  // } else if (activePlatform === "amazon") {
+  //   amazonBtn.removeAttribute("disabled");
+  }
+});
+
+function resetBtnState() {
+  redditBtn.setAttribute("disabled", "disabled");
+  mediumBtn.setAttribute("disabled", "disabled");
+  // amazonBtn.setAttribute("disabled", "disabled");
+}
+
+let activeTabId = "";
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  var activeTab = tabs[0];
+  activeTabId = activeTab.id;
+});
+
+function startReddit() {
+  if (!activeTabId) {
+    console.log("Active Tab ID missing!");
+    return;
+  }
+
+  chrome.scripting.executeScript(
+    {
+      target: { tabId: activeTabId },
+      files: ["js/reddit.js"],
+    },
+    function () {
+      redditBtn.innerHTML = "Stop";
+      redditBtn.onclick = stopScriptExecution;
+    }
+  );
+}
+
+function stopScriptExecution() {
+  console.log("Stop script execution");
+  chrome.tabs.sendMessage(activeTabId, {
+    message: "___clapper_stop_script",
+  });
+
+  if (activePlatform === "reddit") {
+    redditBtn.innerHTML = "Start";
+    redditBtn.onclick = startReddit;
+  }
 }
