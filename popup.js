4bb1a65130ac0331d571b2bd5f69d38316d38d83
Yashu Mittal <mittalyashu77@gmail.com>
2024-03-20 23:07:15 +0530

    feat: set explicit name for redditSpeedInput

--- popup.js
+++ popup.js
@@ -3,21 +3,21 @@ const defaultSpeed = 1000;
 
 // Read speed from Chrome storage
 chrome.storage.local.get(["speed"], function (result) {
-  const speedInput = document.getElementById("speedInput");
+  const redditSpeedInput = document.getElementById("redditSpeedInput");
 
   if (result?.speed === undefined) {
-    speedInput.value = defaultSpeed;
+    redditSpeedInput.value = defaultSpeed;
     chrome.storage.local.set({ speed: defaultSpeed }, function () {
       console.log("Speed is set to " + defaultSpeed);
     });
   } else {
-    speedInput.value = result.speed;
+    redditSpeedInput.value = result.speed;
   }
 });
 
 // Set speed from the Popup
-document.getElementById("setSpeed").onclick = function () {
-  var speed = document.getElementById("speedInput").value;
+document.getElementById("redditSpeedInputBtn").onclick = function () {
+  var speed = document.getElementById("redditSpeedInput").value;
   chrome.storage.local.set({ speed: speed }, function () {
     console.log("Speed is set to " + speed);
   });
